c37e3a07249501a359738da82fabbc0f41c0e870
Gabriel <g_machado1@hotmail.com>
2021-09-22 11:22:22 -0300

    Implemented all controllers, useCases and repositories

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,20 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers;
+
+        try {
+            const users = this.listAllUsersUseCase.execute({
+                user_id: user_id.toString(),
+            });
+            return response.send(users);
+        } catch (err) {
+            return response.status(400).send({ error: err.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,18 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+
+        try {
+            const user = this.showUserProfileUseCase.execute({ user_id });
+            return response.send(user);
+        } catch (err) {
+            return response.status(404).send({ error: err.message });
+        }
+    }
 }
 
 export { ShowUserProfileController };
